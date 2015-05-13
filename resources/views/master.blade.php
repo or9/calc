922687ed9f53d6d8584b05dac25c164a89cb3d2c
abdurrahman <abdur.rahman@lochbridge.com>
2015-05-13 11:18:51 -0400

    css @media

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -2,6 +2,8 @@
 <html>
 	<head>
 		<meta charset="utf-8" />
+		<meta name="viewport"
+			content="width=device-width, initial-scale=1" />
 		{{-- <meta name="csrf-token" content="{{ csrf_token() }}" /> --}}
 		<script src="components/bower_components/webcomponentsjs/webcomponents.min.js"></script>
 		<script src="js/app.js"></script>
